c69f2078f5489202cfcb2e5c56c1c776cc04d7c6
tannpsy <pasyaintan@gmail.com>
2025-05-09 10:06:37 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import './App.css';
+import {
+  FaLinkedin,
+  FaInstagram,
+  FaPhone,
+  FaShoppingCart,
+  FaEnvelope,
+} from 'react-icons/fa';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="header-left">
+          <div className="logo">
+            <img src="/logo3.png" alt="PresUniv" className="logo-img" />
+          </div>
+          <nav className="nav">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#" className="icon-link">
+              <FaShoppingCart className="icon" />
+            </a>
+          </nav>
+        </div>
+
+        <div className="header-right">
+          <button className="sign-in">Sign In</button>
+          <button className="get-started">Get Started</button>
+        </div>
+      </header>
+
+      <main className="main">
+        <div className="hero-text">
+          <h1 className="gradient-text">
+            MERCHANDISE <br /> CENTER
+          </h1>
+          <button className="see-merch">See Our Merch</button>
+        </div>
+        <div className="hero-logo">
+          <img src="/logo2.png" alt="PresUniv Merchandise Center" className="hero-img" />
+        </div>
+      </main>
+
+      <footer className="footer">
+        <p>Don't See What Are You Looking For?</p>
+        <button className="contact-us">
+          <FaEnvelope className="icon" /> Contact Us
+        </button>
+        <div className="social-icons">
+          <FaLinkedin className="icon" />
+          <FaInstagram className="icon" />
+          <FaPhone className="icon" />
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
